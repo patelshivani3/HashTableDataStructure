59b4b72e21256a1e9295ecafaf7ea5baee987f21
patelshivani3 <shivpatel3001@gmail.com>
2023-01-12 16:25:03 +0530

    Modified Hash Table to find the word in sentence

--- HashTableDataStructure/Program.cs
+++ HashTableDataStructure/Program.cs
@@ -11,7 +11,29 @@ namespace HashTableDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Hash Table Data Structure");
+            string paragraph = "To be or not to be";
+            CountNumbOfOccurence(paragraph);
             Console.ReadLine();
+        }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("Displaying after add operation");
+            hashTabe.Display();
+            //string s = "or";
+            //hashTabe.Remove(s);
+            //Console.WriteLine("After removed an item {0}", s);
+            //hashTabe.Display();
         }
     }
 }
